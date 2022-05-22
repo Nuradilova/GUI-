00e39731288aab5d2b61951427a803aa4314cc7b
Burma <99819603+Nuradilova@users.noreply.github.com>
2022-05-22 14:07:56 +0600

    Create code.py

---
+++ code.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+root = Tk()
+root.geometry('500x500')
+
+#def openFile():
+ #   filepath = filedialog.askopenfilename(initialdir='C:\\Users\\user\\python files', title='Open file', 
+  #                                          filetypes=(('image files', "*.jpg"), ("all files", "*.*")) )
+    
+   # file = open(filepath, 'r')
+    #print(file.read())
+    #file.close()
+
+
+
+button_quit = Button(root, text = "Quit Button", command = root.quit)
+button_quit.grid(row=3, column=0)
+
+button_1 = Button(root, text= "Button1")
+button_2 = Button(root, text= "Button2")
+
+button_1.grid(row=1, column=0)
+button_2.grid(row=2, column=0)
+#
+
+my_img = ImageTk.PhotoImage(Image.open("icon.png"))
+my_label = Label(image=my_img)
+my_label.grid(row=4, column=0)
+
+
+
+#button = Button(text = 'open', command = openFile)
+#button.pack()
+root.mainloop()
+
+
+
+
